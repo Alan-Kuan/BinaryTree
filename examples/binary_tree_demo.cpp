dcbf95d6d1a0b231e736775a768a9c59fb62e0cc
Alan-Kuan <kuanhaoan@gmail.com>
2020-02-07 17:57:50 +0800

    Added a demo in the examples directory.

---
+++ examples/binary_tree_demo.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "../include/binary_tree.hpp"
+
+using namespace std;
+
+using namespace Nlib;
+
+namespace Cmd {
+
+	enum {
+
+		exit,
+		add,
+		clear,
+		print
+
+	};
+
+}
+
+int main(void){
+
+	BinaryTree<int> bt;
+
+	int cmd, new_data;
+
+	do{
+
+		cout << Cmd::exit  << ": exit, ";
+		cout << Cmd::add   << ": add, ";
+		cout << Cmd::clear << ": clear, ";
+		cout << Cmd::print << ": print" << endl;
+
+		cout << "Enter the command code: ";
+
+		cin >> cmd;
+
+		if(cmd == Cmd::add){
+
+			cout << "Enter the new data: ";
+
+			cin >> new_data;
+
+		}
+
+		switch(cmd){
+
+			case Cmd::exit:
+				break;
+			case Cmd::add:
+				bt.add(new_data) ? cout << "Successfully added " << new_data << '.' << endl : cout << "Failed: memory allocation failed." << endl;
+				break;
+			case Cmd::clear:
+				bt.clear();
+				cout << "Successfully cleared." << endl;
+				break;
+			case Cmd::print:
+				bt.print();
+				break;
+			default:
+				cout << "Wrong command!" << endl;
+
+		}
+
+	}while(cmd != Cmd::exit);
+
+	return 0;
+
+}
